54f4d6a948f074ef8aa474380a9f966d93ea66af
Ubuntu <ubuntu@ip-172-31-9-60.us-west-1.compute.internal>
2022-04-29 17:27:27 +0000

    instance changes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.get('/loaderio-d151b3c073a8fa1fb89af9e2701e71d6.txt', (req, res) => {
+	res.send('loaderio-d151b3c073a8fa1fb89af9e2701e71d6')
+		})
+
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
